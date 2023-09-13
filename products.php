f0d3572f8403f6f39952df11aec298beba756776
Gelberm <gelber.mahecha@skyline.be>
2023-09-12 21:27:43 -0400

    Initial version

---
+++ products.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MySQL Table Viewer</title>
+</head>
+<body>
+<h1>MySQL Table Viewer</h1>
+<?php
+// Define database connection variables
+$servername = "gl-mysql-server.mysql.database.azure.com";
+$username = "gladmin";
+$password = "samlog790!";
+$dbname = "gl";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query database for all rows in the table
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Display table headers
+    echo "<table><tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th></tr>";
+    // Loop through results and display each row in the table
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["description"] . "</td><td>$ " . $row["price"] . "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+// Close database connection
+$conn->close();
+?>
+</body>
+</html>
